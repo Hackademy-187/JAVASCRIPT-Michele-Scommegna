3c267daec63b8e38ff7911cc9239272827cd0952
Michele Scommegna <michelescommegna63@gmail.com>
2026-04-16 15:48:03 +0200

    JAVASCRIPT

---
+++ main.js
@@ -0,0 +1,72 @@
+// esercizio n1:
+let num1 = 7;
+let num2 = 11;
+console.log(num1);
+console.log(num2);
+
+let music = "play";
+console.log(music);
+
+music = "streaming";
+console.log(music);
+
+const p_grieco = 3.14159265359;
+console.log(p_grieco);
+
+p_grieco = 69;
+console.log(p_grieco);
+
+// esercizio n2:
+// let num_A = 10;
+// let num_B = 20;
+// let num_C = 45;
+// let num_D = 11;
+// let num_E = 77;
+
+// let somma = num_A + num_B + num_C + num_D + num_E;
+// console.log(somma);
+
+// let media = somma/5;
+// console.log(media);
+
+// console.log("La somma tra i numeri equivale a " + somma + " e la media equivale a " + media);
+
+
+// esrcizio n3:
+
+// let annoCorrente = 2026;
+// let annoDinascita = 1996;
+// let età = annoCorrente - annoDinascita;
+// console.log(età);
+
+// let arrivoEtà = 100 - età;
+// console.log(arrivoEtà);
+
+// console.log("hai " + età + " e ti mancano " + arrivoEtà + "anni per essere più vecchio");
+
+// let bktk = `Hai ${età} e ti mancano ${arrivoEtà} anni per essere più vecchio di primo`;
+// console.log(bktk);
+
+// esercizio n4:
+// let numeroA = Number (prompt("inserisci numero1"));
+// let numeroB = Number (prompt("inserisci numero2"));
+// console.log(numeroA);
+// console.log(numeroB);
+
+// let somma = numeroA + numeroB;
+// let sottrazione = numeroA - numeroB;
+// let moltiplicazione = numeroA * numeroB;
+// let divisione = numeroA / numeroB;
+// let potenza = numeroA ** numeroB;
+
+// let stampConsole = `Con i numeri da te scelti, i risultati delle varie operazioni sono: ${somma}, ${sottrazione}, ${moltiplicazione}, ${divisione}, ${potenza}.`
+// console.log(stampConsole);
+
+// console.log("AL POSTO DI STARE A FARE I CONTI VAI A GIOCARE ALL'APERTO!");
+
+
+
+
+
+
+ 
